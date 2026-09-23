7b3b9158817cb5c2f5f7ff64a9d197385e3fd607
Goku <goku@example.com>
2026-09-23 15:41:01 +0530

    Add robots.txt for SEO

---
+++ src/app/robots.ts
@@ -0,0 +1,11 @@
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: 'https://simon-riley17.github.io/dev-tools-directory/sitemap.xml',
+  }
+}
